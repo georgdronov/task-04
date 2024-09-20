39b58a377c02f3592b4b9a586e123654522ac7d6
Georg Dronov <dronovgeorg72@gmail.com>
2024-09-20 20:55:34 +0300

    update logs for test

--- routes/auth.js
+++ routes/auth.js
@@ -32,7 +32,7 @@ router.post("/register", async (req, res) => {
     res.status(201).json({ message: "User registered successfully", token });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Server error" });
+    res.status(500).json({ message: "Server error Auth.JS", error });
   }
 });
 
