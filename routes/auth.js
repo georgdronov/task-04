8783d7005b74c87ec6a466ad41b803962931789e
Georg Dronov <dronovgeorg72@gmail.com>
2024-09-21 16:38:47 +0300

    update middleware

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const db = require("../db");
+const authenticateTokenAndCheckStatus = require("../authMiddleware");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
@@ -17,16 +18,14 @@ router.post("/register", async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-
     const token = jwt.sign({ userId: users.insertId }, process.env.JWT_SECRET, {
-      expiresIn: "1h", // 1 hour live time
+      expiresIn: "1h",
     });
 
     await db.query(
       "INSERT INTO users (email, password, token) VALUES (?, ?, ?)",
       [email, hashedPassword, token]
     );
-
     res.status(201).json({ message: "User registered successfully", token });
   } catch (error) {
     console.error(error);
@@ -60,14 +59,16 @@ router.post("/login", async (req, res) => {
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
-
     await db.query("UPDATE users SET token = ? WHERE id = ?", [token, user.id]);
-
     res.status(200).json({ message: "Login successful", token });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
   }
+});
+
+router.get("/check-auth", authenticateTokenAndCheckStatus, (req, res) => {
+  res.json({ isAuthenticated: true });
 });
 
 module.exports = router;
