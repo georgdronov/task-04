603579f2fb9fea4621c5ed574914865b69f9bf7e
Georg Dronov <dronovgeorg72@gmail.com>
2024-09-21 13:37:43 +0300

    update DB.js

--- db.js
+++ db.js
@@ -5,16 +5,25 @@ console.log("DB_HOST:", process.env.DB_HOST);
 console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 120000,
 });
 
-pool.getConnection()
+pool
+  .getConnection()
   .then(() => console.log("Connected to the MySQL database"))
-  .catch(err => console.error("Error connecting to the database:", err.message));
+  .catch((err) =>
+    console.error("Error connecting to the database:", err.message)
+  );
 
 module.exports = pool;
